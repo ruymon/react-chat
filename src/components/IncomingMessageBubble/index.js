cec59cdbce7677227075b27ccfb3455d9d782cd1
ruymon <ruystfrancis@gmail.com>
2021-12-05 18:17:13 -0300

    Senders avatar feature

--- src/components/IncomingMessageBubble/index.js
+++ src/components/IncomingMessageBubble/index.js
@@ -8,7 +8,6 @@ export function IncomingMessageBubble({messageData, userData}) {
   const {avatar, firstName, lastName} = userData;
   return ( 
     <div className="flex my-2 gap-x-3">
-      {/* TODO: Keep Senders Image on Chat - Use Avatar Component */}
       <Avatar avatarUrl={avatar} />
       <div className="bg-blue-100 w-3/5 p-2 rounded-md text-gray-800 flex flex-col">
         <span className="mb-1 font-bold text-gray-800">{`${firstName} ${lastName}`}</span>
