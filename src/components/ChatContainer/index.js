3733f35e0bdfc4fbeb695733fc1bd29be0f23b2a
ruymon <ruystfrancis@gmail.com>
2021-12-05 17:51:27 -0300

    Incoming messages sender info

--- src/components/ChatContainer/index.js
+++ src/components/ChatContainer/index.js
@@ -11,15 +11,15 @@ export function ChatContainer() {
   const sender = chatRoomInfo[2];
   return (
     <div className="mt-2 py-4 flex flex-col overflow-y-auto h-96">
-      {messages.map(({message, id, pilotid, sent}) => {
-        if (sender == pilotid) {
-
+      {messages.map(({message, id, pilotid, sent, read}) => {
+        if (`${sender}` === `${pilotid}`) {
+          console.log(message.read);
           // TODO: Set isRead
-          // TODO: Set sentBy in IncomingMessageBubble
-          return <OutgoingMessageBubble key={id} message={message} sendTimestamp={sent}/>;
+          return <OutgoingMessageBubble key={id} message={message} sendTimestamp={sent} isRead/>;
         } else {
-          console.log(participants[pilotid]);
-          return <IncomingMessageBubble key={id} message={message} sendTimestamp={sent} avatarUrl={'https://www.github.com/ruymon.png'} sentBy='Shirley Teresinha'/>;
+          if (!participants[pilotid]) return null;
+          const {avatar, firstName, lastName} = participants[pilotid];
+          return <IncomingMessageBubble key={id} message={message} sendTimestamp={sent} avatarUrl={avatar} sentBy={`${firstName} ${lastName}`}/>;
         }
       })}
     </div>
