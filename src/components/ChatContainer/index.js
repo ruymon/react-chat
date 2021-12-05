17d63abe46a4eb2d39ad147165db67cf6a8df3f0
ruymon <ruystfrancis@gmail.com>
2021-12-05 08:32:45 -0300

    TIRED STRESS SHIT! (this dosent work...)

--- src/components/ChatContainer/index.js
+++ src/components/ChatContainer/index.js
@@ -8,19 +8,18 @@ export function ChatContainer() {
   const participants = useRecoilValue(participantsState);
   const messages = useRecoilValue(messagesState);
 
-
   const sender = chatRoomInfo[2];
-
   return (
     <div className="mt-2 py-4 flex flex-col overflow-y-auto h-96">
       {messages.map(({message, id, pilotid, sent}) => {
-        if (sender === pilotid) {
+        if (sender == pilotid) {
 
           // TODO: Set isRead
           // TODO: Set sentBy in IncomingMessageBubble
           return <OutgoingMessageBubble key={id} message={message} sendTimestamp={sent}/>;
         } else {
-          return <IncomingMessageBubble key={id} message={message} sendTimestamp={sent} avatarUrl="https:\/\/flyazulv.com\/imagecrop\/croppedavatar.php?piloco=AZU2234&firstname=K&lastname=F" sentBy='Shirley Teresinha'/>;
+          console.log(participants[pilotid]);
+          return <IncomingMessageBubble key={id} message={message} sendTimestamp={sent} avatarUrl={'https://www.github.com/ruymon.png'} sentBy='Shirley Teresinha'/>;
         }
       })}
     </div>
